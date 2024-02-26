c8f963060ca7f7517e4e0aebf08730a95ca6f343
linerafaela <128156711+linerafaela@users.noreply.github.com>
2024-02-26 14:24:28 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -8,10 +8,13 @@ chuteString = input('Digite um número: ')
 
 print('Você digitou o número: ',chuteString)
 
-chute = int (chuteString)
+chute = int(chuteString)
 
 if numeroSecreto == chute:
     print('Você acertou!!!!')
-
+elif(chute>numeroSecreto):
+        print('Você errou!! O número secreto é um nùmero menor')
 else:
-    print('Você errou!!!')
+    print('Você errou!!! O número secreto é um número maior')
+
+    #Aula Elif 26.02.24
